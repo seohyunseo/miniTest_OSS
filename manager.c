c84e8d5636f87f0e9cd3d4a964caaf548b8377b0
seohyunseo <21700358@handong.edu>
2021-04-25 21:29:46 +0900

    add saveData func

--- manager.c
+++ manager.c
@@ -41,6 +41,10 @@ void saveData(Product p[], int count){
 	//중량 가격 제품명
 	fp= fopen("product.txt","wt");
 	
+	for(int i = 0; i < count; i++){
+	if(p[i].weight == -1 || p[i].price == -1) continue;
+	fprintf(fp, "%-15s %3d %4d \n",p[i].name,p[i].weight,p[i].price);
+	}	
 	
 	fclose(fp);
 	printf("저장됨!\n");
